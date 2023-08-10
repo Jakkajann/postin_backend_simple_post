7a11fd4572aa12f31fd0a7d787460d891d28b946
Fernando Mocrosky <fernandobismaia@gmail.com>
2023-08-10 00:58:47 -0300

    Finalize simple post app without delete and update

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
 
 const app = express();
 
@@ -13,15 +13,41 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.route("/")
+
     .get((req, res) => {
         res.render("index", {posts: posts});
     });
 
+
 app.route("/createPost")
+
     .get((req, res) => {
         res.render("createPost");
+    })
+
+    .post((req, res) => {
+        const postTitle = req.body.postTitle;
+        const postContent = req.body.postContent;
+        const newPost = {
+            title: postTitle.trim(),
+            content: postContent
+        };
+        posts.push(newPost);
+        res.redirect("/");
     });
 
+
+app.route("/posts/:postTitle")
+
+    .get((req, res) => {
+       const post = posts.filter(post => {
+            const rTitle = req.params.postTitle.replaceAll(" ", "-").trim();
+            const pTitle = post.title.replaceAll(" ", "-");
+            return pTitle === rTitle;
+       });
+        res.render("posts", {post: post[0]});
+    })
+;
 
 app.listen(3000, () => {
     console.log("Server running on port 3000");
